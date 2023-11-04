284be5962892f9a5b52982a2863789ed1ee71692
Alan Grainger <alan.grainger@gmail.com>
2023-11-04 21:48:33 +0100

    Catch errors

--- src/Compressor.ts
+++ src/Compressor.ts
@@ -15,43 +15,45 @@ async function compressArrayBuffer (data: ArrayBuffer, mimeType: string, options
 }
 
 export async function compressImage (data: ArrayBuffer, filetype: string) {
+  const originalData = data
   const type = types[filetype]
-  let changed = false
 
   // Only compress supported images, and only if above 100Kb
   if (type && data.byteLength > 100 * 1024) {
-    const defaultOptions = {
-      maxSizeMB: 0.7,
-      maxWidthOrHeight: 1400,
-      preserveExif: false,
-      initialQuality: 0.6,
-      fileType: type
-    }
-
-    const originalData = data
-    data = await compressArrayBuffer(data, type, defaultOptions)
-
-    // If size is >200Kb, test compressing the file as JPG and see how the size compares
-    if (data.byteLength > 200 * 1024) {
-      const test = await compressArrayBuffer(data, type, Object.assign(defaultOptions, {
-        fileType: 'image/jpeg'
-      }))
-      if (test.byteLength < data.byteLength) {
-        data = test
-        filetype = 'jpg'
+    try {
+      const defaultOptions = {
+        maxSizeMB: 0.7,
+        maxWidthOrHeight: 1400,
+        preserveExif: false,
+        initialQuality: 0.6,
+        fileType: type
       }
-    }
 
-    if (data.byteLength > originalData.byteLength) {
-      // New file is bigger, return the original file
-      data = originalData
+      data = await compressArrayBuffer(data, type, defaultOptions)
+
+      // If size is >200Kb, test compressing the file as JPG and see how the size compares
+      if (data.byteLength > 200 * 1024) {
+        const test = await compressArrayBuffer(data, type, Object.assign(defaultOptions, {
+          fileType: 'image/jpeg'
+        }))
+        if (test.byteLength < data.byteLength) {
+          data = test
+          filetype = 'jpg'
+        }
+      }
+
+      if (data.byteLength > originalData.byteLength) {
+        // New file is bigger, return the original file
+        data = originalData
+      }
+    } catch (e) {
+      console.log(e)
     }
-    changed = data.byteLength !== originalData.byteLength
   }
 
   return {
     data,
     filetype,
-    changed
+    changed: data.byteLength !== originalData.byteLength
   }
 }
